b563e71fcf38581618f072fb78ec5fdbdaa347ba
Tijani Ayomide <ayomidetijani6@gmail.com>
2022-07-03 15:57:41 +0100

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express"
 import expressLayouts from "express-ejs-layouts"
 import indexHandler from "../src/routes/index.route"
 import authHandler from "../src/routes/auth.route"
-
+import path from "path"
 const app = express()
 
 app.set('view engine', 'ejs')
@@ -10,7 +10,7 @@ app.set('layout', '../views/layouts/main.ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(expressLayouts)
 
 
