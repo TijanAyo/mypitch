3d2ba3c5dcef669b2076fdd007b39f0003562a2a
Tijani Ayomide <ayomidetijani6@gmail.com>
2022-07-03 14:02:31 +0100

    remove

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 const PORT = process.env.PORT || 5000
 
 
-process.env.NODE_ENV !== "production" && server.on("listening", () => {
+server.on("listening", () => {
     console.log(`listening on http://localhost:${PORT}`);
     connectDB()
 });
